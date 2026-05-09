da2d7f269ecea3f9685f23925766d531e002ced8
CristinaPineda <cristinadaspineda@yahoo.com.br>
2026-05-09 18:43:55 -0300

    :rocket: configs para role root

--- infra/env/dev/terraform.tfvars
+++ infra/env/dev/terraform.tfvars
@@ -1,8 +1,28 @@
 # NÃO REMOVER OU SUBSTITUIR ESSAS VARIÁVEIS
 # Elas são essenciais para o deploy e precisam existir neste arquivo.
 
-environment = "dev"
-project_name = ""
-aws_region = "sa-east-1"
+environment  = "dev"
+project_name = "cidade-refugio"
+aws_region   = "sa-east-1"
+
+###########################################################################################
+# GITHUB OIDC CONFIGURATION
+###########################################################################################
+
+github_repository = "vojolabs/projeto-cdr-backend"
+
+###########################################################################################
+# DATABASE CREDENTIALS
+# IMPORTANTE: Estes valores devem ser substituídos com valores seguros
+# Considere usar AWS Secrets Manager ou variáveis de ambiente
+###########################################################################################
+
+database_credentials = {
+  username = "admin"
+  password = "CHANGE_ME_SECURE_PASSWORD"  # Substituir com senha segura
+}
+
+# Criar secrets no Secrets Manager
+create_secrets_manager = true
 
 ###########################################################################################
---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b51137000000000000000000000000000000000"] 
+}
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:vojolabs/*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-central-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_attach" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
