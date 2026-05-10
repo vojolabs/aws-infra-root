dbb1fccac35f310aa196b58027d33775452bfce1
CristinaPineda <cristinadaspineda@yahoo.com.br>
2026-05-10 19:11:04 -0300

    ajustando recursos

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,9 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["1b51137000000000000000000000000000000000"] 
+  thumbprint_list = ["1b5113705c3460acbafbc2b3c5c5ded81c5c8a5e"] # Thumbprint oficial do GitHub OIDC
 }
+
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -16,9 +17,10 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:vojolabs/*"]
+      values   = [
+        "repo:vojolabs/*:ref:refs/heads/*", # Permite todos os repositórios da org vojolabs
+      ]
     }
-
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
@@ -27,28 +29,12 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
   }
 }
 
-resource "aws_iam_role" "github_actions_role" {
-  name               = "github-actions-central-role"
+resource "aws_iam_role" "github_actions_deploy" {
+  name               = "github-actions-deploy-role"
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-resource "aws_iam_role_policy" "allow_oidc_creation" {
-  name = "AllowOIDCCreation"
-  role = "github-actions-central-role"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "iam:CreateOpenIDConnectProvider"
-        Effect   = "Allow"
-        Resource = "arn:aws:iam::164599051832:oidc-provider/token.actions.githubusercontent.com"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github_actions_attach" {
-  role       = aws_iam_role.github_actions_role.name
+resource "aws_iam_role_policy_attachment" "github_actions_deploy_admin" {
+  role       = aws_iam_role.github_actions_deploy.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
